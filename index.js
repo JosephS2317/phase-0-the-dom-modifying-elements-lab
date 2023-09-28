a3793d1b1eb047661d78fb6ee43e865b8ad57aa8
Joseph <JosephSped23@gmail.com>
2023-09-28 14:12:02 -0400

    creates dom commands to index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code here
+main.remove(main);
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Joe is the champion';
+
+
+
